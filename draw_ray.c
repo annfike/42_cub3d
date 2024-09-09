be35d5a3c836db335e2bdda022d71b5d572e64fe
Dmitrii Miasnikov <dmiasnik@c3r1s3.42lausanne.ch>
2024-09-09 12:04:50 +0200

    norm ray

--- draw_ray.c
+++ draw_ray.c
@@ -3,14 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   draw_ray.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adelaloy <adelaloy@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dmiasnik <dmiasnik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/01 17:19:11 by dmiasnik          #+#    #+#             */
-/*   Updated: 2024/09/07 16:30:05 by adelaloy         ###   ########.fr       */
+/*   Updated: 2024/09/09 12:04:03 by dmiasnik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
+
+void	set_img(t_data *game, float x, float y)
+{
+	game->txt_w = 0;
+	if (x - (int)x <= 0.01)
+	{
+		game->txt_idx = 2;
+		game->txt_w = y - (int)y;
+	}
+	else if (y - (int)y <= 0.01)
+	{
+		game->txt_idx = 0;
+		game->txt_w = x - (int)x;
+	}
+	else if (x - (int)x >= 0.99)
+	{
+		game->txt_idx = 3;
+		game->txt_w = 1 - (y - (int)y);
+	}
+	else if (y - (int)y >= 0.99)
+	{
+		game->txt_idx = 1;
+		game->txt_w = 1 - (x - (int)x);
+	}
+}
 
 float	ft_ray(t_data *game, float look)
 {
@@ -18,8 +43,6 @@ float	ft_ray(t_data *game, float look)
 	float	maxray;
 	float	x;
 	float	y;
-	float	dif_x;
-	float	dif_y;
 
 	maxray = sqrt(pow(game->map_game_height, 2)
 			+ pow(game->map_game_width_max, 2));
@@ -30,29 +53,7 @@ float	ft_ray(t_data *game, float look)
 		y = game->y + c * sin(look);
 		if (game->map_game[(int)floor(y)][(int)floor(x)] == '1')
 		{
-			dif_x = x - (int)x;
-			dif_y = y - (int)y;
-			game->txt_w = 0;
-			if (dif_x <= 0.01)
-			{
-				game->txt_idx = 2;
-				game->txt_w = dif_y;
-			}
-			else if (dif_y <= 0.01)
-			{
-				game->txt_idx = 0;
-				game->txt_w = dif_x;
-			}
-			else if (dif_x >= 0.99)
-			{
-				game->txt_idx = 3;
-				game->txt_w = 1 - dif_y;
-			}
-			else if (dif_y >= 0.99)
-			{
-				game->txt_idx = 1;
-				game->txt_w = 1 - dif_x;
-			}
+			set_img(game, x, y);
 			break ;
 		}
 		c += 0.01;
@@ -67,23 +68,22 @@ void	draw_line(t_data *game, int x, float dist, t_img img)
 	unsigned int	wall_h;
 	float			txt_h;
 	float			step;
-	t_img			img_txt;
 
 	wall_h = WIN_HEIGHT / dist;
 	txt_h = 0.0f;
-	img_txt = game->images[game->txt_idx];
-	step = (float)img_txt.height / wall_h;
+	step = (float)game->images[game->txt_idx].height / wall_h;
 	if (wall_h > WIN_HEIGHT)
 	{
-		txt_h = 0.5f * (wall_h - WIN_HEIGHT) / wall_h * img_txt.height;
+		txt_h = 0.5f * (wall_h - WIN_HEIGHT) / wall_h
+			* game->images[game->txt_idx].height;
 		wall_h = WIN_HEIGHT;
 	}
-	src = img_txt.addr;
-	src += (int)((float)game->txt_w * img_txt.width);
+	src = game->images[game->txt_idx].addr;
+	src += (int)((float)game->txt_w * game->images[game->txt_idx].width);
 	dst = img.addr + x + (WIN_HEIGHT - wall_h) / 2 * WIN_WIDTH;
 	while (wall_h-- > 0)
 	{
-		*dst = *(src + ((int)txt_h) * img_txt.width);
+		*dst = *(src + ((int)txt_h) * game->images[game->txt_idx].width);
 		dst += WIN_WIDTH;
 		txt_h += step;
 	}
